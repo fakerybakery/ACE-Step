b1168cba322a9083c2083f22350c7a304f5b4661
Roberts Slisans <roberts.slisans@gmail.com>
2025-05-07 11:35:55 +0300

    fix README encoding in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="ace_step",
     description="ACE Step: A Step Towards Music Generation Foundation Model",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     version="0.1.0",
     packages=["acestep"],
